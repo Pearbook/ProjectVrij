aadec0890ca2a288eddcc9cd4624cc88b71516bb
Pearbook <renskuilman@gmail.com>
2019-05-14 22:23:19 +0200

    Decal painting added

---
+++ Paint/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Transform MyCamera;
+
+    public float GraffitiRange;
+    public LayerMask GraffitiMask;
+
+    public GameObject GraffitiPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            SprayGraffiti();
+    }
+
+    void SprayGraffiti()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(MyCamera.position, MyCamera.forward, out hit, GraffitiRange, GraffitiMask))
+        {
+            if(hit.collider != null)
+            {
+                GameObject prefab = (GameObject)Instantiate(GraffitiPrefab, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+}
